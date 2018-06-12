8477ca8c288dbd85da106c20369c48b59e27e9b0
lihaixing <lihaixing@yycunluo.coom>
2018-06-12 11:57:48 +0800

    css过滤完成

--- TreeShaking/webpack.config.js
+++ TreeShaking/webpack.config.js
@@ -1,5 +1,9 @@
 var path = require('path')
-var webpack=require('webpack')
+var webpack = require('webpack')
+var PurifyCss = require('purifycss-webpack')
+// 处理css多路径问题
+var glob = require('glob-all')
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: {
         app: './src/app.js'
@@ -12,60 +16,23 @@ module.exports = {
     },
     module: {
         rules: [
-            // {
-            //     test: /\.js$/,
-            //     use: [
-            //         {
-            //             loader: 'babel-loader',
-            //             options: {
-            //                 presets: ['env'],
-            //                 plugins: ['lodash']
-            //             }
-            //         }
-            //     ]
-            // },
             {
-                test: /\.css$/,
+                test: /\.js$/,
                 use: [
                     {
-                        // loader: 'style-loader/useable',
-                        loader: 'style-loader',
+                        loader: 'babel-loader',
                         options: {
-                            // 插入到dom 有几个css文件就会有几个style标签
-                            insertInto: '#app',
-                            // 只能有一个style标签
-                            singleton: true,
-                            // 形变函数，浏览器中执行js时，对css的处理
-                            transform: './css.transform.js'
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true,
-                            modules: true,
-                            // class命名规则
-                            localIdentName: '[path][name]_[local]_[hash:base64:5]'
-                        }
-                    },
-                    // postcss 放在css-loader之前
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require('postcss-cssnext')()
-                            ]
+                            presets: ['env'],
+                            plugins: ['lodash']
                         }
                     }
                 ]
             },
             {
-                test: /\.less$/,
-                use: [
-                    // 注意loader是从后往前执行的，先是less转成css，然后css嵌入到js中，最后将css嵌入到html中
-                    {
-                        // loader: 'style-loader/useable',
+                test: /\.css$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    // 如果没有提取出来，用style-loader嵌入到html中
+                    fallback: {
                         loader: 'style-loader',
                         options: {
                             // 插入到dom 有几个css文件就会有几个style标签
@@ -76,32 +43,67 @@ module.exports = {
                             transform: './css.transform.js'
                         }
                     },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            // minimize: true,
-                            // modules:true,
-                            // class命名规则
-                            // localIdentName:'[path][name]_[local]_[hash:base64:5]'
+                    // 如果提取出来，怎么处理
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // minimize: true,
+                                modules:true,
+                                // class命名规则
+                                localIdentName:'[path][name]_[local]_[hash:base64:5]'
+                            }
                         }
-                    },  // postcss 放在css-loader之前
-                    {
-                        loader: 'postcss-loader',
+                    ]
+                })
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    // 如果没有提取出来，用style-loader嵌入到html中
+                    fallback: {
+                        loader: 'style-loader',
                         options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require('postcss-cssnext')()
-                            ]
+                            // 插入到dom 有几个css文件就会有几个style标签
+                            insertInto: '#app',
+                            // 只能有一个style标签
+                            singleton: true,
+                            // 形变函数，浏览器中执行js时，对css的处理
+                            transform: './css.transform.js'
                         }
                     },
-                    {
-                        loader: 'less-loader'
-                    }
-                ]
+                    // 如果提取出来，怎么处理
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                                // modules:true,
+                                // class命名规则
+                                // localIdentName:'[path][name]_[local]_[hash:base64:5]'
+                            }
+                        },
+                        {
+                            loader: 'less-loader'
+                        }
+                    ]
+                })
             }
         ]
     },
     plugins: [
+        new ExtractTextWebpackPlugin({
+            filename: '[name].min.css',
+            // 默认false 指打包初始化的import的css, 异步的不打包
+            allChunks:false
+        }),
+        new PurifyCss({
+            paths: glob.sync([
+                path.join(__dirname, './*.html'),
+                path.join(__dirname, './src/*.js'),
+                path.join(__dirname, './*.js')
+            ])
+        }),
         // 无用js被去掉，同时代码被压缩
         new webpack.optimize.UglifyJsPlugin()
     ]
